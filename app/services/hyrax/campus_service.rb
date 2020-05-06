506e4e8116000be6709befee41605f328de55b4e
David Walker <dwalker@calstate.edu>
2020-05-06 15:04:20 -0700

    first pass through campus service refactor

--- app/services/hyrax/campus_service.rb
+++ app/services/hyrax/campus_service.rb
@@ -1,23 +1,46 @@
 module Hyrax
   module CampusService
 
+    def self.campuses
+      { bakersfield: 'Bakersfield',
+        chancellor: 'Chancellor',
+        channel: 'Channel Islands',
+        chico: 'Chico',
+        dominguez: 'Dominguez Hills',
+        eastbay: 'East Bay',
+        fresno: 'Fresno',
+        fullerton: 'Fullerton',
+        humboldt: 'Humboldt',
+        longbeach: 'Long Beach',
+        losangeles: 'Los Angeles',
+        maritime: 'Maritime',
+        monterey: 'Monterey Bay',
+        mosslanding: 'Moss Landing',
+        northridge: 'Northridge',
+        pomona: 'Pomona',
+        sacramento: 'Sacramento',
+        sanbernardino: 'San Bernardino',
+        sandiego: 'San Diego',
+        sanfrancisco: 'San Francisco',
+        sanjose: 'San Jose',
+        slo: 'San Luis Obispo',
+        sanmarcos: 'San Marcos',
+        sonoma: 'Sonoma',
+        stanislaus: 'Stanislaus' }
+    end
+
+    def self.get_campus_name_from_id(campus_id)
+      key = campus_id.to_sym
+      raise 'Could not find campus name for id: ' + id unless campuses.key?(key)
+
+      campuses[key]
+    end
+
     def self.get_campus_from_admin_set(admin_set_name)
-
-      campuses = ['Bakersfield', 'Chancellor', 'Channel Islands', 'Chico',
-                  'Dominguez Hills', 'East Bay', 'Fresno', 'Fullerton',
-                  'Humboldt', 'Long Beach', 'Los Angeles', 'Maritime',
-                  'Monterey Bay', 'Moss Landing', 'Northridge', 'Pomona',
-                  'Sacramento', 'San Bernardino', 'San Diego',
-                  'San Francisco', 'San Jose', 'San Luis Obispo',
-                  'San Marcos', 'Sonoma', 'Stanislaus']
-
-      campuses.each do |campus|
-        if admin_set_name.to_s.include?(campus.to_s)
-          return campus.to_s
-        end
+      campuses.values.each do |campus|
+        return campus.to_s if admin_set_name.to_s.include?(campus.to_s)
       end
-
-      'Chancellor' # default if no group selected
+      raise 'No campus admin set could be found'
     end
   end
 end
