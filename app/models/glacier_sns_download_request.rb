871b4dcb5aa072aee62effb77548bbefc85b2dfa
Matthew Clark <winescout@gmail.com>
2020-06-10 18:49:04 -0700

    adds email when unarchive is complete

--- app/models/glacier_sns_download_request.rb
+++ app/models/glacier_sns_download_request.rb
@@ -3,11 +3,18 @@ class GlacierSnsDownloadRequest < ApplicationRecord
   validates :user, :glacier_identifier, presence: true
 
   after_create :make_request
+  before_save :email_link
 
   def make_request 
     response = GlacierUploadService.download(self)
     if response.job_id
       update_attributes(aws_job_identifier: response.job_id, aws_location: response.location)
+    end
+  end
+
+  def email_link
+    if self.is_complete_changed? and self.is_complete?
+      GlacierMailer.unarchive_complete_email(self.user, self.aws_location).deliver_later
     end
   end
 end
