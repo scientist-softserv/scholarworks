b95ab40ead866200a71b7e62d09931d56ce4754a
Matthew Clark <winescout@gmail.com>
2020-06-10 18:54:44 -0700

    adds endpoint for aws glacier unarchive sns

--- app/controllers/glacier_sns_download_requests_controller.rb
+++ app/controllers/glacier_sns_download_requests_controller.rb
@@ -7,4 +7,10 @@ class GlacierSnsDownloadRequestsController < ApplicationController
       render json: download_request.errors, status: 422
     end
   end
+
+  def unarchive_complete_sns
+    # ????? Unsure what the SNS looks like until we get to staging
+    request = GlacierSnsDownloadRequest.last
+    request.update_attribute(:is_complete, true) # triggers email
+  end
 end
