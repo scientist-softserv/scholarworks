1d2e0effbd3304413acae95e8fd8b8dcdac48503
Daniel Lim <daniel@notch8.com>
2020-07-01 15:17:45 -0700

    Solr indexing field for the Handle id (suffix)

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -145,7 +145,7 @@ class CatalogController < ApplicationController
       all_names = config.show_fields.values.map(&:field).join(' ')
       title_name = solr_name('title', :stored_searchable)
       field.solr_parameters = {
-        qf: "#{all_names} file_format_tesim all_text_timv handle_sim",
+        qf: "#{all_names} file_format_tesim all_text_timv handle_sim handle_suffix_sim",
         pf: title_name.to_s
       }
     end
