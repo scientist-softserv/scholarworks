8e2927bcba9e3d725129ccdb619fb78060a5e54f
Alex Boster <boster@tilthouse.org>
2020-10-13 16:40:00 -0700

    Fixes read groups for campus visibility

--- app/models/concerns/hydra/access_controls/campus_visibility.rb
+++ app/models/concerns/hydra/access_controls/campus_visibility.rb
@@ -9,15 +9,16 @@ module Hydra
       end
 
       def visibility=(value)
-        super
-      rescue ArgumentError => error
-        raise error unless value.to_s == Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_CAMPUS
-        raise(RuntimeError, 'No campus is set on this work') if campus.blank?
-        visibility_will_change! unless visibility == Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_CAMPUS
-        campus_slugs = campus.to_a.map { |campus_name| Hyrax::CampusService.get_campus_slug_from_name(campus_name) }
-        remove_groups = represented_visibility - campus_slugs # read from object's campus value
+        if value.to_s == Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_CAMPUS
+          raise RuntimeError, 'No campus is set on this work' if campus.blank?
+          visibility_will_change! unless visibility == Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_CAMPUS
+          campus_slugs = campus.to_a.map { |campus_name| Hyrax::CampusService.get_campus_slug_from_name(campus_name) }
+          remove_groups = represented_visibility - campus_slugs # read from object's campus value
 
-        set_read_groups(campus_slugs, remove_groups)
+          set_read_groups(campus_slugs, remove_groups)
+        else
+          super
+        end
       end
 
       def visibility
