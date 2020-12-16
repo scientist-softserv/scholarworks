5db24b555283515b8fa6a8ff31ae8c7c9fc05b0e
David Walker <dwalker@calstate.edu>
2020-12-15 17:05:37 -0800

    rubocop fixes

--- lib/oai/provider/metadata/dublin_core.rb
+++ lib/oai/provider/metadata/dublin_core.rb
@@ -8,23 +8,20 @@ module OAI::Provider::Metadata
       @schema = 'http://www.openarchives.org/OAI/2.0/oai_dc.xsd'
       @namespace = 'http://www.openarchives.org/OAI/2.0/oai_dc/'
       @element_namespace = 'dc'
-      @fields = [ :title, :creator, :subject, :description, :publisher,
-                  :contributor, :date, :type, :format, :identifier,
-                  :source, :language, :relation, :coverage, :rights,
-                  :campus] # add campus customization
-      Rails.logger.warn @fields.to_s
+      @fields = %i[title creator subject description publisher contributor
+                   date type format identifier source language relation
+                   coverage rights campus] # add campus customization
     end
 
     def header_specification
       {
-          'xmlns:oai_dc' => "http://www.openarchives.org/OAI/2.0/oai_dc/",
-          'xmlns:dc' => "http://purl.org/dc/elements/1.1/",
-          'xmlns:xsi' => "http://www.w3.org/2001/XMLSchema-instance",
-          'xsi:schemaLocation' =>
-              %{http://www.openarchives.org/OAI/2.0/oai_dc/
-            http://www.openarchives.org/OAI/2.0/oai_dc.xsd}.gsub(/\s+/, ' ')
+        'xmlns:oai_dc' => 'http://www.openarchives.org/OAI/2.0/oai_dc/',
+        'xmlns:dc' => 'http://purl.org/dc/elements/1.1/',
+        'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+        'xsi:schemaLocation' =>
+          %(http://www.openarchives.org/OAI/2.0/oai_dc/
+          http://www.openarchives.org/OAI/2.0/oai_dc.xsd).gsub(/\s+/, ' ')
       }
     end
-
   end
 end
