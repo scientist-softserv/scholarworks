3decd410fbe650ffc5066348efecc788db8756bf
David Walker <dwalker@calstate.edu>
2021-02-23 11:30:59 -0800

    throttle takes into account local timezone

--- lib/calstate/metadata.rb
+++ lib/calstate/metadata.rb
@@ -88,14 +88,13 @@ module CalState
     # @return [Boolean]  yes if weekday 9-5
     #
     def self.should_throttle(position, batch = 5)
-      day = Date.today.strftime('%A')
+      now = Time.now.getlocal('-08:00')
 
       # just keep on truckin' over the weekend
-      return false if %w[Saturday Sunday].include? day
+      return false if now.saturday? || now.sunday?
 
       # otherwise throttle 9-5 weekdays
-      hour = Time.now.getlocal('-08:00').hour
-      (position % batch).zero? && (hour >= 9 && hour <= 17)
+      (position % batch).zero? && (now.hour >= 9 && now.hour <= 17)
     end
   end
 end
