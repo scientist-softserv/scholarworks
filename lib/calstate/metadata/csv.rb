4fdd080441ebab177db5de471c074fa5a298981d
David Walker <dwalker@calstate.edu>
2021-04-12 12:30:47 -0700

    metadata transactions

--- lib/calstate/metadata/csv.rb
+++ lib/calstate/metadata/csv.rb
@@ -3,5 +3,6 @@
 require_relative 'csv/utilities'
 require_relative 'csv/fixer'
 require_relative 'csv/reader'
+require_relative 'csv/transaction'
 require_relative 'csv/updater'
 require_relative 'csv/writer'
