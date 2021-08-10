826844f234a88ac65694547a03a87344431a2c26
David Walker <dwalker@calstate.edu>
2021-08-10 15:04:46 -0700

    allow per-work-form department lists

--- app/services/hyrax/departments_service.rb
+++ app/services/hyrax/departments_service.rb
@@ -6,7 +6,17 @@ module Hyrax
     def self.select_options(controller, form)
       campus = Hyrax::CampusService.get_campus_from_form(controller, form)
       campus = campus.downcase.gsub(' ', '_')
-      campus_authority = Qa::Authorities::Local.subauthority_for('departments_' + campus)
+
+      model = model_name(controller)
+      file_name = 'config/authorities/departments_' + campus + '_' + model + '.yml'
+
+      subauthority = if File.exist? file_name
+                       'departments_' + campus + '_' + model
+                     else
+                       'departments_' + campus
+                     end
+
+      campus_authority = Qa::Authorities::Local.subauthority_for(subauthority)
 
       campus_authority.all.map do |element|
         [element[:label], element[:id]]
@@ -24,6 +34,14 @@ module Hyrax
     def self.microdata_type(id)
       return Hyrax.config.microdata_default_type if id.nil?
       Microdata.fetch("resource_type_thesis.#{id}", default: Hyrax.config.microdata_default_type)
+    end
+
+    def self.model_name(controller)
+      name = controller.class.name
+      name.sub!('Hyrax::', '')
+      name.sub!('Controller', '')
+
+      name.downcase
     end
   end
 end
