b177be0362255d6fe3bbd020ef2b032e7e3d292c
David Walker <dwalker@calstate.edu>
2021-11-06 14:46:50 -0700

    handle robots.txt differently per server

--- config/routes.rb
+++ config/routes.rb
@@ -50,5 +50,8 @@ Rails.application.routes.draw do
   end
 
   mount WillowSword::Engine, at: "/sword"
+  get '/robots.txt', to: 'robots#index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
