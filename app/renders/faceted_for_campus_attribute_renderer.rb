7a3c8b44121a4f0a7d1793c340d7d8a9e596a8f7
Bryan Tu <btu@calstate.edu>
2022-09-16 11:21:20 -0700

    Initial check in for facet that uses campus value of the work.

---
+++ app/renders/faceted_for_campus_attribute_renderer.rb
@@ -0,0 +1,12 @@
+require "rails_autolink/helpers"
+
+module Hyrax
+  module Renderers
+    class FacetedForCampusAttributeRenderer < FacetedAttributeRenderer
+      def search_path(value)
+        campus_search_field = ERB::Util.h(Solrizer.solr_name(options.fetch(:search_field, 'campus'), :facetable, type: :string))
+        Rails.application.routes.url_helpers.search_catalog_path(:"f[#{search_field}][]" => value, :"f[#{campus_search_field}]" => options[:campus], locale: I18n.locale)
+      end
+    end
+  end
+end
