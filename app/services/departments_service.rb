b4dfece85d65051ccd5a6c7b60b9fbbddb6518ac
David Walker <dwalker@calstate.edu>
2023-01-18 15:24:32 -0800

    qa clean-up

--- app/services/departments_service.rb
+++ app/services/departments_service.rb
@@ -5,6 +5,6 @@
 #
 class DepartmentsService < AuthorityService
   def initialize(controller)
-    super('departments', controller, true)
+    super('departments', controller, model: true)
   end
 end
