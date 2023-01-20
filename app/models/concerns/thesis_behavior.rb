63fce2dbdb265b80288d96e186e9bfdb855edff6
David Walker <dwalker@calstate.edu>
2023-01-19 20:18:02 -0800

    factor out thesis fields & behavior
    hate doing this, but it addresses circular loading error on the command-line.

---
+++ app/models/concerns/thesis_behavior.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+#
+# Shared behavior for Thesis & Project
+#
+# See also ThesisFields
+#
+module ThesisBehavior
+  extend ActiveSupport::Concern
+
+  included do
+    #
+    # Before saving this work
+    #
+    def on_save
+      set_year
+      set_college
+      set_degree_level
+      set_campus_publisher
+    end
+
+    #
+    # Set the degree level based on the resource type
+    #
+    def set_degree_level
+      service = DegreeLevelService.new(self.class.name.downcase, campus.first)
+      level = service.get(resource_type.first)
+      self.degree_level = level unless level.nil?
+    end
+
+    #
+    # Make the campus the publisher
+    #
+    def set_campus_publisher
+      full_name = CampusService.get_full_from_name(campus.first)
+      self.publisher = [full_name]
+    end
+
+  end
+end
