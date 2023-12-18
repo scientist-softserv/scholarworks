4fa1d71dc6d3d231ac38024b8f9174dc7bcad051
David Walker <dwalker@calstate.edu>
2023-12-18 13:26:30 -0800

    hyrax 3.6 updates for downloads controller

--- app/controllers/hyrax/downloads_controller.rb
+++ app/controllers/hyrax/downloads_controller.rb
@@ -7,6 +7,7 @@ module Hyrax
     include Hydra::Controller::DownloadBehavior
     include Hyrax::LocalFileDownloadsControllerBehavior
     include Scholarworks::DownloadBehavior
+    include Hyrax::WorkflowsHelper # Provides #workflow_restriction?
 
     def self.default_content_path
       :original_file
@@ -23,13 +24,13 @@ module Hyrax
         # For derivatives stored on the local file system
         send_local_content
       else
-        raise ActiveFedora::ObjectNotFoundError
+        raise Hyrax::ObjectNotFoundError
       end
     end
 
     private
 
-      # Override the Hydra::Controller::DownloadBehavior#content_options so that
+    # Override the Hydra::Controller::DownloadBehavior#content_options so that
     # we have an attachement rather than 'inline'
     def content_options
 
@@ -42,57 +43,71 @@ module Hyrax
       ### END CUSTOMIZATION
 
 
-      
+
     end
 
     # Override this method if you want to change the options sent when downloading
-      # a derivative file
-      def derivative_download_options
-        { type: mime_type_for(file), disposition: 'inline' }
-      end
+    # a derivative file
+    def derivative_download_options
+      { type: mime_type_for(file), disposition: 'inline' }
+    end
 
-      # Customize the :read ability in your Ability class, or override this method.
-      # Hydra::Ability#download_permissions can't be used in this case because it assumes
-      # that files are in a LDP basic container, and thus, included in the asset's uri.
-      def authorize_download!
-        authorize! :download, params[asset_param_key]
-      rescue CanCan::AccessDenied
-        unauthorized_image = Rails.root.join("app", "assets", "images", "unauthorized.png")
-        send_file unauthorized_image, status: :unauthorized
-      end
+    def file_set_parent(file_set_id)
+      file_set = Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: file_set_id, use_valkyrie: Hyrax.config.use_valkyrie?)
+      @parent ||=
+        case file_set
+        when Hyrax::Resource
+          Hyrax.query_service.find_parents(resource: file_set).first
+        else
+          file_set.parent
+        end
+    end
 
-      # Overrides Hydra::Controller::DownloadBehavior#load_file, which is hard-coded to assume files are in BasicContainer.
-      # Override this method to change which file is shown.
-      # Loads the file specified by the HTTP parameter `:file`.
-      # If this object does not have a file by that name, return the default file
-      # as returned by {#default_file}
-      # @return [ActiveFedora::File, File, NilClass] Returns the file from the repository or a path to a file on the local file system, if it exists.
-      def load_file
-        file_reference = params[:file]
-        return default_file unless file_reference
+    # Customize the :read ability in your Ability class, or override this method.
+    # Hydra::Ability#download_permissions can't be used in this case because it assumes
+    # that files are in a LDP basic container, and thus, included in the asset's uri.
+    def authorize_download!
+      authorize! :download, params[asset_param_key]
+      # Deny access if the work containing this file is restricted by a workflow
+      return unless workflow_restriction?(file_set_parent(params[asset_param_key]), ability: current_ability)
+      raise Hyrax::WorkflowAuthorizationException
+    rescue CanCan::AccessDenied, Hyrax::WorkflowAuthorizationException
+      unauthorized_image = Rails.root.join("app", "assets", "images", "unauthorized.png")
+      send_file unauthorized_image, status: :unauthorized
+    end
 
-        file_path = Hyrax::DerivativePath.derivative_path_for_reference(params[asset_param_key], file_reference)
-        File.exist?(file_path) ? file_path : nil
-      end
+    # Overrides Hydra::Controller::DownloadBehavior#load_file, which is hard-coded to assume files are in BasicContainer.
+    # Override this method to change which file is shown.
+    # Loads the file specified by the HTTP parameter `:file`.
+    # If this object does not have a file by that name, return the default file
+    # as returned by {#default_file}
+    # @return [ActiveFedora::File, File, NilClass] Returns the file from the repository or a path to a file on the local file system, if it exists.
+    def load_file
+      file_reference = params[:file]
+      return default_file unless file_reference
 
-      def default_file
-        default_file_reference = if asset.class.respond_to?(:default_file_path)
-                                   asset.class.default_file_path
-                                 else
-                                   DownloadsController.default_content_path
-                                 end
-        association = dereference_file(default_file_reference)
-        association&.reader
-      end
+      file_path = Hyrax::DerivativePath.derivative_path_for_reference(params[asset_param_key], file_reference)
+      File.exist?(file_path) ? file_path : nil
+    end
 
-      def mime_type_for(file)
-        MIME::Types.type_for(File.extname(file)).first.content_type
-      end
+    def default_file
+      default_file_reference = if asset.class.respond_to?(:default_file_path)
+                                 asset.class.default_file_path
+                               else
+                                 DownloadsController.default_content_path
+                               end
+      association = dereference_file(default_file_reference)
+      association&.reader
+    end
 
-      def dereference_file(file_reference)
-        return false if file_reference.nil?
-        association = asset.association(file_reference.to_sym)
-        association if association && association.is_a?(ActiveFedora::Associations::SingularAssociation)
-      end
+    def mime_type_for(file)
+      MIME::Types.type_for(File.extname(file)).first.content_type
+    end
+
+    def dereference_file(file_reference)
+      return false if file_reference.nil?
+      association = asset.association(file_reference.to_sym)
+      association if association&.is_a?(ActiveFedora::Associations::SingularAssociation)
+    end
   end
 end
