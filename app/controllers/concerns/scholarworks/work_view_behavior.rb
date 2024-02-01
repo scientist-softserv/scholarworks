49499cf38bd2edd3350516760b1efc1d38108c32
Bryan Tu <btu@calstate.edu>
2024-01-31 16:14:11 -0800

    Check against database instead of using session
    Just check to see if we have a stat with such ip for either work_id or file_id within the last 8 hours.

--- app/controllers/concerns/scholarworks/work_view_behavior.rb
+++ app/controllers/concerns/scholarworks/work_view_behavior.rb
@@ -9,11 +9,11 @@ module Scholarworks
 
       def track_view
         # ignore bots, other weird requests & multiple views during same session
-        return if StatsService.bad_user_agent?(request)
-        return unless session["stats_work_view_#{params[:id]}"].nil?
+        return if StatsService.bad_user_agent?(request) || params[:id].nil?
 
-        # track visit in session and database
-        session["stats_work_view_#{params[:id]}"] = true
+        stats = StatsWorkView.check_active(request.remote_ip, params[:id])
+        return unless stats.empty?
+
         curation_concern = @curation_concern ? @curation_concern : _curation_concern_type.find(params[:id])
         stats_work_view = StatsWorkView.new(work_id: params[:id],
                                             ip_address: request.remote_ip,
